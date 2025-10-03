a7dbb230786d6d75e504ea1cdd32ae4cd03269f8
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2025-10-03 12:50:12 +0700

    refactor(core): rearrange middleware execution order
    - move errorhandler to the end of middleware stack

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,12 +1,12 @@
 import express, { type Application, type Request, type Response } from 'express';
-import { errorHandler } from '../middleware/errorHandler.js';
 import apiRoutes from '../routes/index.js';
+import { errorHandler } from '../middleware/errorHandler.js';
 
 const app: Application = express();
 
 app.use(express.json());
-app.use(errorHandler);
 app.use('/api/v1', apiRoutes);
+app.use(errorHandler);
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Welcome to Recova API!' });
