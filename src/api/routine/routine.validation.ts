adc4155d5f96e46bec5612acbd2ab7ca9e036127
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2025-10-03 07:13:34 +0700

    fix(form): allow empty content in daily checkin
    - make content optional in daily checkin form
    - refine content validation to allow empty strings

--- src/api/routine/routine.validation.ts
+++ src/api/routine/routine.validation.ts
@@ -3,7 +3,10 @@ import { z } from 'zod/v4';
 export const dailyCheckinSchema = z.object({
   body: z.object({
     mood: z.string().min(1, { message: 'Mood is required' }),
-    isSuccessful: z.boolean('IsSuccessful must be a boolean'),
-    content: z.string().min(1, { message: 'Content is required' }).optional(),
+    isSuccessful: z.boolean(),
+    content: z
+      .string()
+      .optional()
+      .refine(val => !val || val.length > 0, { message: 'Content is required' }),
   }),
 });
