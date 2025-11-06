72dc8a9d2adb3576be85c3960c1026847d64e51f
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2025-11-06 23:27:25 +0700

    chore(config): add node environment configuration
    - add node environment variable to .env.example
    - load node environment variable into config

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 const config = {
   port: process.env.PORT || 3000,
+  nodeEnv: process.env.NODE_ENV || 'development',
   databaseUrl: process.env.DATABASE_URL || '',
   docsUrl: process.env.DOCS_URL || '',
   jwt: {
